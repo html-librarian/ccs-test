56bb89edf091e3556aaa8edb03dcda2adf68ee11
Max Golev <m.mudi@list.ru>
2023-01-23 03:48:34 +0300

    Add Fonts and fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
@@ -10,7 +9,6 @@ export default defineConfig({
       scss: {
         sourceMap: false,
         additionalData: `
-            @import "./src/assets/scss/_fonts.scss";
             @import "./src/assets/scss/_mixins.scss";
         `,
       },
